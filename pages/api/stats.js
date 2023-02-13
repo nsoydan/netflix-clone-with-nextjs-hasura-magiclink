55093f99bbc1d9c70d815a29ceeed1efb663103b
nihat soydan <nsoydan@gmail.com>
2023-02-13 17:43:26 +0300

    testing update and insert stats is OK

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,16 +1,43 @@
 import jwt from "jsonwebtoken";
-
+import {
+  findVideoIdByUser,
+  insertStats,
+  updateStats,
+} from "../../lib/db/hasura";
 export default async function stats(req, res) {
   if (req.method === "POST") {
     console.log({ cookies: req.cookies });
     try {
+      const videoId = req.query.videoId;
       const token = req.cookies.token;
       if (!req.cookies.token) {
         res.status(403).send({});
       } else {
-        var decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log({ decoded });
-        res.send({ msg: "it works", decoded });
+        var decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        console.log({ decodedToken });
+        const userId = decodedToken.issuer;
+
+        const doesStatsExit = await findVideoIdByUser(userId, videoId, token);
+
+        if (doesStatsExit) {
+          const updatedStats = await updateStats(token, {
+            favourited: 55,
+            userId,
+            watched: true,
+            videoId: "gxc6y2ZVfCU",
+          });
+          console.log({ updatedStats });
+          res.send({ msg: "it works,  stats updated", updatedStats });
+        } else {
+          const insertedStats = await insertStats(token, {
+            favourited,
+            userId,
+            watched,
+            videoId,
+          });
+          console.log({ insertedStats });
+          res.send({ msg: "it works, stats inserted", insertedStats });
+        }
       }
     } catch (error) {
       console.log("Error occured /stats", error);
