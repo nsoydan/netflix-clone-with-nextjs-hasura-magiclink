11f62cc0e4b4617fa655f6a808fb9b286df70112
nihat soydan <nsoydan@gmail.com>
2023-02-17 14:11:34 +0300

    fixed http to https in iframe in video.js

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -105,7 +105,7 @@ const Video = ({ video }) => {
           type="text/html"
           width="100%"
           height="390"
-          src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`}
+          src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`}
           frameBorder="0"
         />
         <div className={styles.likeDislikeBtnWrapper}>
